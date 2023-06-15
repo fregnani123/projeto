f4e5dd96ee033a8fb23f8e730b2dbb77127e9530
fregnani123 <fabianofregnani@hotmail.com>
2023-06-14 21:02:42 -0300

    atualizar

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
-const path = require('path')
 const PORT = 3000;
+const routes = require('./routes')
 
-
-app.use(express.static(path.join(__dirname,'public')))
-
-
+app.use(routes.app)
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta:${PORT}`);
