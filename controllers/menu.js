109615f50e1cfdb06d5eacfe88ea6f91178f23ae
fregnani123 <fabianofregnani@hotmail.com>
2023-06-23 17:42:27 -0300

    estudos

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,4 +1,5 @@
-const btnMobile = document.querySelector('#btn-mobile').addEventListener('click',() =>{
+const btnMobile = document.querySelector('#btn-mobile').addEventListener('click',(event) =>{
+    if (event.type === 'touchstart') event.preventDefault()
     const nav = document.querySelector('#nav')
   nav.classList.toggle('active');
 })
