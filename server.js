2007dfc45cb392733d72f3e9dd79710cf5ff9c7c
fregnani123 <fabianofregnani@hotmail.com>
2023-06-24 21:06:30 -0300

    estudos

--- server.js
+++ server.js
@@ -5,16 +5,13 @@ const app = express();
 const PORT = 3000;
 const routes = require('./routes')
 
-
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use(routes);
+app.use(routes); 
 
 app.get('/menu.js',(req,res)=>{
     res.sendFile(path.join(__dirname,'controllers/menu.js'))
 });
-
-
 
 app.listen(PORT,()=>{
     console.log('Servidor rodando na PORT: '+ PORT)
