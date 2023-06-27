708964e19b1466491012150eea6409c1721a930a
fregnani123 <fabianofregnani@hotmail.com>
2023-06-26 22:32:54 -0300

    estudos

--- routes.js
+++ routes.js
@@ -2,8 +2,9 @@ const express = require('express')
 const route = express.Router();
 const homeController = require('./controllers/homeController')
 
-route.get('/teste', homeController.paginaTeste)
-route.get('/home', homeController.paginaHome)
+route.get('/teste', homeController.paginaTeste);
+route.get('/home', homeController.paginaHome);
+route.get('/')
 
 
 
